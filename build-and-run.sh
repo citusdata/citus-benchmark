84276f99cf3357d318b76910d3c4f0f7ab29d598
Marco Slot <marco@citusdata.com>
2020-02-14 12:59:13 +0100

    Update build-and-run.sh

--- build-and-run.sh
+++ build-and-run.sh
@@ -40,7 +40,7 @@ psql -v "ON_ERROR_STOP=1" -f sql/ch-benchmark-tables.sql
 psql -f sql/ch-benchmark-distribute.sql
 
 # build hammerdb related tables
-(cd $HOME/HammerDB-3.3 && time ./hammerdbcli auto $current_dir/build.tcl 2>&1 | tee -a ./results/build_${file_name}.log)
+(cd $HOME/HammerDB-3.3 && time ./hammerdbcli auto $current_dir/build.tcl 2>&1 | tee -a $current_dir/results/build_${file_name}.log)
 
 # distribute tpcc tables in cluster
 psql -f sql/tpcc-distribute.sql
@@ -61,7 +61,7 @@ fi
 
 if [ $is_tpcc = true ] ; then
     # run hammerdb tpcc benchmark
-    (cd $HOME/HammerDB-3.3 && ./hammerdbcli auto $current_dir/run.tcl | tee -a ./results/run_${file_name}.log)
+    (cd $HOME/HammerDB-3.3 && ./hammerdbcli auto $current_dir/run.tcl | tee -a $current_dir/results/run_${file_name}.log)
     # filter and save the NOPM( new orders per minute) to a new file
     grep -oP '[0-9]+(?= NOPM)' ./results/run_${file_name}.log >> ./results/${file_name}_NOPM.log
 else
