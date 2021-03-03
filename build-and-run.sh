6aa64d43e0728f68cd296341d26729d751a77e98
Marco Slot <marco@citusdata.com>
2021-03-03 15:00:00 +0100

    Update build-and-run.sh

--- build-and-run.sh
+++ build-and-run.sh
@@ -13,8 +13,8 @@ if [ $# -eq 4 ] ; then
 else version="4.0"
 fi
 file_name=$1
-is_tpcc=$2
-is_ch=$3
+is_tpcc=${2:-true}
+is_ch=${3:-false}
 
 export PGHOST=${PGHOST:-localhost}
 export PGPORT=${PGPORT:-5432}
@@ -27,13 +27,16 @@ mkdir -p results/
 # drop tables if they exist since we might be running hammerdb multiple times with different configs
 psql -v "ON_ERROR_STOP=1" -f sql/drop-tables.sql
 
-# create ch-benchmark tables in cluster
-psql -v "ON_ERROR_STOP=1" -f sql/ch-benchmark-tables.sql
+if [ $is_ch = true ] ; then
+  # create ch-benchmark tables in cluster
+  psql -v "ON_ERROR_STOP=1" -f sql/ch-benchmark-tables.sql
 
-# distribute ch-benchmark tables
-psql -f sql/ch-benchmark-distribute.sql
+  # distribute ch-benchmark tables
+  psql -f sql/ch-benchmark-distribute.sql
+fi
 
-# set shard count
+# set Citus configurations
+psql -c "ALTER ROLE current_user SET citus.replication_model TO 'streaming'" 2>/dev/null || true
 psql -c "ALTER ROLE current_user SET citus.shard_count TO 40" 2>/dev/null || true
 psql -c "ALTER ROLE current_user SET citus.enable_repartition_joins to on" 2>/dev/null || true
 
