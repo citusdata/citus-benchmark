068383258d1a5a2a6adb566473ced1b611da59aa
Jelte Fennema <github-tech@jeltef.nl>
2021-12-15 16:17:59 +0100

    Add a way to use HammerDB with a few custom citus changes
    I created a couple of PRs to upstream HammerDB. It will take some time
    before these PRs are merged and then released in HammerDB 4.4. In the
    mean time I've created a branch on our fork of the HammerDB repo, that
    has all these improvements.
    
    The most important improvements are:
    1. Official support for Citus, that can be turned on and off with a
       single setting.
    2. Change the way the dataset is built to use COPY instead of multi-row
       inserts. This speeds up building the dataset significant.

--- generate-hammerdb.sh
+++ generate-hammerdb.sh
@@ -2,6 +2,13 @@
 set -euo pipefail
 
 cd "$(dirname "$0")"
-OUTPUT=$(./download-hammerdb.sh "$1")
-tar -xf "$OUTPUT"
-patch -p0 < "HammerDB-patch/$1.patch"
+
+if [ "$1" = "4.3-custom" ]; then
+    ./generate-hammerdb.sh 4.3
+    git clone https://github.com/citusdata/HammerDB --branch custom-4.3 HammerDB-4.3-custom
+    cp -R HammerDB-4.3/{lib,include,bin} HammerDB-4.3-custom
+else
+    OUTPUT=$(./download-hammerdb.sh "$1")
+    tar -xf "$OUTPUT"
+    patch -p0 < "HammerDB-patch/$1.patch"
+fi
