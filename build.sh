f0a41d24dc00c68f7766a2a091247ffb37106ccd
Jelte Fennema <github-tech@jeltef.nl>
2021-12-17 10:35:43 +0100

    Use getopts for nicer argument interface
    The shell scripts to build and run HammerDB did not have very user
    friendly arguments. This changes the arguments they take, make usage of
    flags using `getopts` and allow you to run the scripts without any
    arguments at all.

--- build.sh
+++ build.sh
@@ -1,51 +1,34 @@
 #!/bin/bash
 
 # Usage:
-# ./build.sh [HammerDB version] {file_name} {is_tpcc} {is_tpch}
-# Example:
-# ./build.sh 4.3 myfirstbenchmark true false
+# ./build.sh [--hammerdb-version[=]<version>] [--ch|--ch-queries-only] [--no-citus] [--name[=]name]
 
-# fail if trying to reference a variable that is not set.
-set -u
-# exit immediately if a command fails
-set -e
-# echo commands
-set -x
-# fail if a command that is piped fails
-set -o pipefail
-
-if [ $# -eq 4 ] ; then
-    version=$1
-    shift
-else version="4.3"
-fi
-file_name=$1
-# is_tpcc=${2:-true} # unused in this script currently
-is_ch=${3:-false}
-
-export PGHOST=${PGHOST:-localhost}
-export PGPORT=${PGPORT:-5432}
-export PGUSER=${PGUSER:-postgres}
-export PGDATABASE=${PGDATABASE:-$PGUSER}
-export PGPASSWORD=${PGPASSWORD}
-
+source parse-arguments.sh
 mkdir -p results/
 
 # drop tables if they exist since we might be running hammerdb multiple times with different configs
 psql -v "ON_ERROR_STOP=1" -f sql/drop-tables.sql
 
-if [ "$is_ch" = true ] ; then
-  # create ch-benchmark tables in cluster
-  psql -v "ON_ERROR_STOP=1" -f sql/ch-benchmark-tables.sql
-
-  # distribute ch-benchmark tables
-  psql -f sql/ch-benchmark-distribute.sql
-fi
-
 # set Citus configurations
 psql -c "ALTER ROLE current_user SET citus.shard_count TO 40" 2>/dev/null || true
 psql -c "ALTER ROLE current_user SET citus.enable_repartition_joins to on" 2>/dev/null || true
 
+sed -i.sedbak -e "s/pg_cituscompat .*/pg_cituscompat $IS_CITUS/" build.tcl
+rm build.tcl.sedbak
+
 # build hammerdb related tables
-test -d "HammerDB-$version" || ./generate-hammerdb.sh "$version"
-(cd HammerDB-$version && time ./hammerdbcli auto ../build.tcl | tee "../results/hammerdb_build_${file_name}.log")
+test -d "HammerDB-$HAMMERDB_VERSION" || ./generate-hammerdb.sh "$HAMMERDB_VERSION"
+(cd "HammerDB-$HAMMERDB_VERSION" && time ./hammerdbcli auto ../build.tcl | tee "../results/hammerdb_build_${BENCHNAME}.log")
+
+# Needs to be done after building TPC tables, otherwise HammerDB complains that
+# tables already exist in the database.
+if [ "$IS_CH" = true ] ; then
+    # create ch-benchmark tables in cluster
+    psql -v "ON_ERROR_STOP=1" -f sql/ch-benchmark-tables.sql
+
+    if [ "$IS_CITUS" = true ]; then
+        # distribute ch-benchmark tables
+        psql -f sql/ch-benchmark-distribute.sql
+    fi
+fi
+
