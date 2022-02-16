924bb4dc827e9c6b521167f13ef8772a7e651e90
ccfelius <lottefelius@hotmail.com>
2022-02-16 22:39:44 +0100

    take benchmark_name as argument

--- install-and-run-ycsb.sh
+++ install-and-run-ycsb.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export BENCHMARK_NAME={1:-fixed}
+
 sudo apt install -y default-jre python postgresql-client-common postgresql-client-12
 
 wget https://github.com/brianfrankcooper/YCSB/releases/download/0.17.0/ycsb-0.17.0.tar.gz
