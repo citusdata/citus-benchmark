ed5991a45dd8ec2134694bc5670db712f372cce8
ccfelius <lottefelius@hotmail.com>
2022-02-17 12:43:44 +0100

    removed parse arguments

--- build-and-run-ycsb.sh
+++ build-and-run-ycsb.sh
@@ -3,11 +3,9 @@
 # Usage:
 # ./build-and-run.sh [--hammerdb-version[=]<version>] [--ch|--ch-queries-only] [--no-citus] [--name[=]name] [--shard-count[=]<shard_count>]
 
-source parse-arguments.sh
 
-BENCHMARK_NAME=fixed
 export HOMEDIR=$PWD
-export BENCHMARK_NAME=${BENCHMARK_NAME}-output
+export BENCHMARK_NAME=output
 export PGDATABASE=citus
 
 # BENCHMARK_NAME=${1:-standard}
@@ -24,6 +22,4 @@ export PGDATABASE=citus
 
 mkdir -p $BENCHMARK_NAME
 
-source parse-arguments.sh
-
-./install-and-run-ycsb.sh $BENCHMARK_NAME
+sh -ex ./install-and-run-ycsb.sh $BENCHMARK_NAME
