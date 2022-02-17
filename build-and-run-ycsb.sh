abfa84d965a15c0c3fb993b3bcf232b45f6ba8f1
ccfelius <lottefelius@hotmail.com>
2022-02-17 10:06:55 +0100

    export PGDB

--- build-and-run-ycsb.sh
+++ build-and-run-ycsb.sh
@@ -8,6 +8,19 @@ source parse-arguments.sh
 BENCHMARK_NAME=fixed
 export HOMEDIR=$PWD
 export BENCHMARK_NAME=${BENCHMARK_NAME}-output
+export PGDATABASE=citus
+
+# BENCHMARK_NAME=${1:-standard}
+# CLUSTER_NAME=${CLUSTER_NAME:-lottecitus}
+# PGPASSWORD=${PGPASSWORD:-thesis123$}
+
+# export CLUSTER_NAME
+# export PGPASSWORD
+# export PGHOST=c.$CLUSTER_NAME.postgres.database.azure.com
+# export PGDATABASE=citus
+# export PGUSER=citus
+# export HOMEDIR=$PWD
+# export BENCHMARK_NAME=${BENCHMARK_NAME}-output
 
 mkdir -p $BENCHMARK_NAME
 
