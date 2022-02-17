2cf9ecd1ee6a9937d7931e1f0e93544f0011c490
ccfelius <lottefelius@hotmail.com>
2022-02-17 20:58:06 +0100

    cleaned file

--- build-and-run-ycsb.sh
+++ build-and-run-ycsb.sh
@@ -3,22 +3,9 @@
 # Usage:
 # ./build-and-run.sh [--hammerdb-version[=]<version>] [--ch|--ch-queries-only] [--no-citus] [--name[=]name] [--shard-count[=]<shard_count>]
 
-
 export HOMEDIR=$PWD
 export BENCHMARK_NAME=output
 export PGDATABASE=citus
-
-# BENCHMARK_NAME=${1:-standard}
-# CLUSTER_NAME=${CLUSTER_NAME:-lottecitus}
-# PGPASSWORD=${PGPASSWORD:-thesis123$}
-
-# export CLUSTER_NAME
-# export PGPASSWORD
-# export PGHOST=c.$CLUSTER_NAME.postgres.database.azure.com
-# export PGDATABASE=citus
-# export PGUSER=citus
-# export HOMEDIR=$PWD
-# export BENCHMARK_NAME=${BENCHMARK_NAME}-output
 
 mkdir -p $BENCHMARK_NAME
 
