5ad7d9cf9777ede42a5bad2d5d7d8b2ae92a42b5
ccfelius <lottefelius@hotmail.com>
2022-02-17 14:45:33 +0100

    stay in current directory

--- create-csv-ycsb.sh
+++ create-csv-ycsb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-cd "$(dirname "$0")"
+# cd "$(dirname "$0")"
 
-# check whether csv file exists in directory
+# check whether csv file exists in current directory
 compgen -G "*.csv" > /dev/null || { >&2 echo No run has finished yet ; exit 1 ; }
 for run_file in *.csv; do
     echo $(cat $run_file)
