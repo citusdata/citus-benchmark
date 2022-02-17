cf9a6eeb7503f807fd7e515d154ae1fe1c6f5b13
ccfelius <lottefelius@hotmail.com>
2022-02-17 17:29:59 +0100

    changedir to current name

--- create-csv-ycsb.sh
+++ create-csv-ycsb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-# cd "$(dirname "$0")"
+cd "$(dirname "$0")"
 
 # check whether csv file exists in current directory
 compgen -G "*.csv" > /dev/null || { >&2 echo No run has finished yet ; exit 1 ; }
