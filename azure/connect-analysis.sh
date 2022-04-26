bbd4cd116fd3caa35101be7dc0f17bf76db318d9
ccfelius <lottefelius@hotmail.com>
2022-04-26 14:39:43 +0200

    find ip from perf. driver

--- azure/connect-analysis.sh
+++ azure/connect-analysis.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 RESOURCE_GROUP=$1
+export VMNAME="${RESOURCE_GROUP}-driver-analysis"
+echo $VMNAME
 
-ip=$(az deployment group show --resource-group "$RESOURCE_GROUP" --name "$RESOURCE_GROUP" --query properties.outputs.AnalysisDriverPublicIp.value --output tsv)
+# ip=$(az deployment group show --resource-group "$RESOURCE_GROUP" --name "$RESOURCE_GROUP" --query properties.outputs.PublicIp.value --output tsv)
+ip=$(az vm list-ip-addresses --name "$VMNAME" --resource-group "$RESOURCE_GROUP" --query "[].virtualMachine.network.publicIpAddresses[0].ipAddress" --output tsv)
+echo "Trying to connect with VM for performance monitoring..."
 ssh "$ip"
