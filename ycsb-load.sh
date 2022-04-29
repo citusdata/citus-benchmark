21c3f0c6c335cbd81db27335d1b5bb7951340eba
ccfelius <lottefelius@hotmail.com>
2022-04-29 10:45:35 +0200

    including loadbalancing

--- ycsb-load.sh
+++ ycsb-load.sh
@@ -8,4 +8,4 @@ export THREADS=$5
 
 cd ycsb-0.17.0
 
-bin/ycsb load jdbc -P workloads/$WORKLOAD -p db.driver=org.postgresql.Driver -p recordcount=$RECORDS -p threadcount=$THREADS -cp ./postgresql-42.2.14.jar -p db.url="jdbc:postgresql://localhost:$PORT/$DATABASE/?prepareThreshold=0" | tee ${HOMEDIR}/${OUTDIR}/load_${WORKLOAD}_${THREAD}_${RECORDS}.log
+bin/ycsb load jdbc -P workloads/$WORKLOAD -p db.driver=org.postgresql.Driver -p recordcount=$RECORDS -p threadcount=$THREADS -cp ./postgresql-42.2.14.jar -p db.url="jdbc:postgresql://localhost:$PORT/$DATABASE/?loadBalanceHosts=true" | tee ${HOMEDIR}/${OUTDIR}/load_${WORKLOAD}_${THREAD}_${RECORDS}.log
--- ycsb-run.sh
+++ ycsb-run.sh
@@ -9,4 +9,4 @@ export OPERATIONS=$6
 
 cd ycsb-0.17.0
 
-bin/ycsb run jdbc -P workloads/$WORKLOAD -p db.driver=org.postgresql.Driver -p recordcount=$RECORDS -p operationcount=$OPERATIONS -p threadcount=$THREADS -cp ./postgresql-42.2.14.jar -p db.url="jdbc:postgresql://localhost:$PORT/$DATABASE/?prepareThreshold=0" | tee ${HOMEDIR}/${OUTDIR}/run_${WORKLOAD}_${THREAD}_${RECORDS}_${OPERATIONS}.log
+bin/ycsb run jdbc -P workloads/$WORKLOAD -p db.driver=org.postgresql.Driver -p recordcount=$RECORDS -p operationcount=$OPERATIONS -p threadcount=$THREADS -cp ./postgresql-42.2.14.jar -p db.url="jdbc:postgresql://localhost:$PORT/$DATABASE/?loadBalanceHosts=true" | tee ${HOMEDIR}/${OUTDIR}/run_${WORKLOAD}_${THREAD}_${RECORDS}_${OPERATIONS}.log
