91da9af2c3dfd6927528575fb630d524a1cc6ddd
ccfelius <lottefelius@hotmail.com>
2022-04-29 14:33:54 +0200

    save citus host in .bashrc

--- get-citus-host.sh
+++ get-citus-host.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export CITUS_HOST=`psql -tAX -c "select string_agg(substring(nodename from 9),',') from pg_dist_node where groupid > 0 or (select count(*) from pg_dist_node) = 1"`
+echo export CITUS_HOST=`psql -tAX -c "select string_agg(substring(nodename from 9),',') from pg_dist_node where groupid > 0 or (select count(*) from pg_dist_node) = 1"`  >> .bashrc
