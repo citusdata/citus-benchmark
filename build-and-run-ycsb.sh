e3193e195eaaf5ec33815a43cf47c3eb853d6d9f
ccfelius <lottefelius@hotmail.com>
2022-04-29 10:44:43 +0200

    python arguments

--- build-and-run-ycsb.sh
+++ build-and-run-ycsb.sh
@@ -10,3 +10,5 @@ export SHARD_COUNT=$3
 mkdir -p $OUTPUT_FOLDER
 
 ./install-and-run-ycsb.sh $OUTPUT_FOLDER $RECORDS $OPERATIONS $SHARD_COUNT
+
+# python3 run-benchmark.py --outdir=$OUTPUT_FOLDER --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT 
