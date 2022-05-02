b458849d277bb189cb1d9758e255e6acdfa96496
ccfelius <lottefelius@hotmail.com>
2022-05-02 12:20:15 +0200

    cleaned code

--- run-benchmark.py
+++ run-benchmark.py
@@ -5,10 +5,6 @@ import sys
 import pandas as pd
 import time
 
-# TODO: ONEXIT
-# TODO: IF error
-
-
 def eprint(*args, **kwargs):
 
     """
@@ -45,15 +41,12 @@ class Benchmark(object):
     def parse_workloads(self, workloads):
 
         """
-        parses string of workloads seperated by a semicolon
+        parses string of workloads seperated by a comma
         returns list of workloads
         """
 
-        if ";" not in workloads:
-
+        if type(thread_counts) == int:
             return [self.check_workloadname(workloads)]
-
-        workloads = set(workloads.split(';'))
 
         # Raise error if there are any workloads that do not exist
         if workloads.intersection(set(self.YCSB_WORKLOADS)) != workloads:
@@ -79,28 +72,8 @@ class Benchmark(object):
 
     def parse_threadcounts(self, thread_counts):
 
-        # 300 plain (example) still runs incorrectly
-
         """
-        parses string of threadcounts seperated by a semicolon
-        returns list of threadcounts
-        """
-
-        # truncate string if it ends with ";"
-        if ";" == thread_counts[-1]:
-            thread_counts = thread_counts[:-1]
-
-        # throw error if non-integer arguments
-        if ";" not in str(thread_counts):
-            return [self.check_if_int(thread_counts)]
-
-        return [self.check_if_int(thread) for thread in thread_counts.split(';')]
-
-
-    def parse_threadcounts_list(self, thread_counts):
-
-        """
-        Parses string of threadcounts in list format
+        Parses string of threadcounts and returns list 
         """
 
         if type(thread_counts) == int:
@@ -124,24 +97,22 @@ class Benchmark(object):
 
     def install_jdbc(self):
 
-        """ install jdbc postgresql driver """
+        """ install JDBC PostgreSQL driver """
 
-        # Check if postgresql jdbv driver exists
+        # Check if postgresql jdbc driver exists
         if os.path.isfile('ycsb-0.17.0/postgresql-42.2.14.jar'):
             return
 
         os.chdir("ycsb-0.17.0")
-
         run(['wget', 'https://jdbc.postgresql.org/download/postgresql-42.2.14.jar'], shell = False)
-
         os.chdir(self.HOMEDIR)
 
 
     def __init__(self, workloadname = "workloada", threads = "248", records = 1000, operations = 10000, port = "5432", database = "citus",
-    outdir = "output", workloadtype = "load", workloads="workloada", iterations = 1, outputfile = "results.csv", citus = True, shard_count = 64):
+    outdir = "output", workloadtype = "load", workloads="workloada", iterations = 1, outputfile = "results.csv", shard_count = 16):
 
         self.HOMEDIR = os.getcwd()
-        self.THREADS = self.parse_threadcounts_list(threads)
+        self.THREADS = self.parse_threadcounts(threads)
         self.YCSB_WORKLOADS = ["workloada", "workloadb", "workloadc", "workloadf", "workloadd", "workloade"]
         self.RECORDS = records
         self.OPERATIONS = operations
@@ -244,46 +215,14 @@ class Benchmark(object):
             self.single_workload()
 
 
-    def load_workloada(self):
+    def run_workload(self, workloadname, workloadtype):
 
         """
-        runs workload e (loading and running)
+        runs a workload and set params accordingly
         """
 
-        self.WORKLOAD_NAME = "workloada"
-        self.WORKLOAD_TYPE = "load"
-
-        self.single_workload()
-
-        self.WORKLOAD_TYPE = "run"
-    
-    
-    def run_workloadc(self):
-
-        """
-        runs workload e (loading and running)
-        """
-
-        self.load_workloada()
-
-        self.WORKLOAD_NAME = "workloadc"
-        self.WORKLOAD_TYPE = "run"
-
-        self.single_workload()
-
-
-    def run_workloade(self):
-
-        """
-        runs workload e (loading and running)
-        """
-
-        self.WORKLOAD_NAME = "workloade"
-        self.WORKLOAD_TYPE = "load"
-
-        self.single_workload()
-
-        self.WORKLOAD_TYPE = "run"
+        self.WORKLOAD_NAME = workloadname
+        self.WORKLOAD_TYPE = workloadtype
         self.single_workload()
 
     
@@ -305,8 +244,8 @@ class Benchmark(object):
 
             for thread in self.THREADS:
                 self.CURRENT_THREAD = thread
-                self.load_workloada()
-                self.run_workloadc()
+                self.run_workload("workloada", "load")
+                self.run_workload("workloadc", "run")
 
             print(f"Done running workloadc for iteration {i}")
             print("Generating CSV")
@@ -324,20 +263,22 @@ class Benchmark(object):
         # if list contains only workloade then load and run workloade
         if workloads == ['workloade']:
             self.WORKLOAD_NAME = workloads[0]
-            self.run_workloade()
+            self.run_workload("workloade", "load")
+            self.run_workload("workloade", "run")
 
         # Sort workloads in 'YCSB Core Workloads' order
         workloads = self.sort_workloads(workloads)
 
         # DB needs to be loaded so always load with workloada
-        self.load_workloada()
+        self.run_workload("workloade", "load")
 
         # Iterate through all workloads
         for workload in workloads:
             self.WORKLOAD_NAME = workload
 
             if self.WORKLOAD_NAME == "workloade":
-                self.run_workloade()
+                self.run_workload("workloade", "load")
+                self.run_workload("workloade", "run")
                 continue
 
             self.WORKLOAD_NAME = workload
@@ -368,15 +309,16 @@ class Benchmark(object):
                     self.WORKLOAD_NAME = workload
 
                     if workload == "workloada":
-                        self.load_workloada()
-                        self.single_workload()
+                        self.run_workload("workloada", "load")
+                        self.run_workload("workloada", "run")
                         continue
 
                     if workload == "workloade":
-                        self.run_workloade()
+                        self.run_workload("workloade", "load")
+                        self.run_workload("workloade", "run")
                         continue
                     
-                    self.single_workload()
+                    self.run_workload(workload, "run")
             
             print("Done running all YCSB core workloads")
             print("Generating CSV")
