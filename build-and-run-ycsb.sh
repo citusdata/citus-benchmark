efc94f801f6cc464d97fd46f226aed87af9f7723
ccfelius <lottefelius@hotmail.com>
2022-05-07 11:50:38 +0200

    dynamic iterations param added

--- build-and-run-ycsb.sh
+++ build-and-run-ycsb.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 export HOMEDIR=$PWD
-export OUTPUT_FOLDER=output
+export OUTPUT_FOLDER=largeexp
 export PGDATABASE=citus
 export RECORDS=$1
 export OPERATIONS=$2
 export SHARD_COUNT=$3
 export THREAD_COUNT=$4
+export ITERATIONS=$5
 
 mkdir -p $OUTPUT_FOLDER
 
-# ./install-and-run-ycsb.sh $OUTPUT_FOLDER $RECORDS $OPERATIONS $SHARD_COUNT
-
-python3 run-benchmark.py --outdir=$OUTPUT_FOLDER --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT citus_workload
+python3 run-benchmark.py --outdir=$OUTPUT_FOLDER --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS citus_workload 
