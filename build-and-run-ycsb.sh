8d332e8692f564b79484cf69183137483d02d50c
ccfelius <lottefelius@hotmail.com>
2022-05-08 12:46:04 +0200

    resource, workers params

--- build-and-run-ycsb.sh
+++ build-and-run-ycsb.sh
@@ -8,7 +8,9 @@ export OPERATIONS=$2
 export SHARD_COUNT=$3
 export THREAD_COUNT=$4
 export ITERATIONS=$5
+export WORKERS=$6
+export RESOURCE_GROUP=$5
 
 mkdir -p $OUTPUT_FOLDER
 
-python3 run-benchmark.py --outdir=$OUTPUT_FOLDER --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS citus_workload 
+python3 run-benchmark.py --outdir=$OUTPUT_FOLDER --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP citus_workload 
