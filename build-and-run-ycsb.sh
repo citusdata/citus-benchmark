fda7456cac3e87bbb2420a62032d287a61b7f7d3
ccfelius <lottefelius@hotmail.com>
2022-05-18 13:05:17 +0200

    added monitor params

--- build-and-run-ycsb.sh
+++ build-and-run-ycsb.sh
@@ -10,7 +10,8 @@ export THREAD_COUNT=$4
 export ITERATIONS=$5
 export WORKERS=$6
 export RESOURCE_GROUP=$7
+export MONITORPW=$8
 
 mkdir -p $OUTPUT_FOLDER
 
-python3 run-benchmark.py --outdir=$OUTPUT_FOLDER --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP citus_workload
+python3 benchmark.py --outdir=$OUTPUT_FOLDER --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW citus_workload
