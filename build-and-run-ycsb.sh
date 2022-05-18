488f111d05cacd49a8814b7664e9e10f5a5164c7
ccfelius <lottefelius@hotmail.com>
2022-05-18 13:06:26 +0200

    added monitor params and test parallel load

--- build-and-run-ycsb.sh
+++ build-and-run-ycsb.sh
@@ -14,4 +14,4 @@ export MONITORPW=$8
 
 mkdir -p $OUTPUT_FOLDER
 
-python3 benchmark.py --outdir=$OUTPUT_FOLDER --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW citus_workload
+python3 benchmark.py --outdir=$OUTPUT_FOLDER --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW test_parallel_load
