525ddb54c138972df227182e71b19f890549c4f3
ccfelius <lottefelius@hotmail.com>
2022-05-20 11:13:00 +0200

    typo

--- azure/run-benchmark-ycsb.sh
+++ azure/run-benchmark-ycsb.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 export RESOURCE_GROUP=$1
 
 ./create-drivervm.sh "$@"
-echo "Createn of Driver VM succeeded"
+echo "Creation of Driver VM succeeded"
 # ./wait-for-results-ycsb.sh "$RESOURCE_GROUP" 2> /dev/null
 # ./cleanup.sh "$RESOURCE_GROUP"
 
