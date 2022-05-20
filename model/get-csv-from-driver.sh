ea49e678d8bacfade54b0d55010c7fe5b57f08a4
ccfelius <lottefelius@hotmail.com>
2022-05-20 10:18:48 +0200

    untrack model folder

--- model/get-csv-from-driver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-RESOURCE_GROUP=$1
-OUTPUT_FOLDER=$2
-
-ip=$(az deployment group show --resource-group "$RESOURCE_GROUP" --name "$RESOURCE_GROUP" --query properties.outputs.driverPublicIp.value --output tsv)
-
-scp -r $ip:citus-benchmark/*.csv $OUTPUT_FOLDER
