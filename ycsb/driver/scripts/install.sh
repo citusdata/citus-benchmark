796bc36ff3a2b6b787a8424d8abef35e307517b6
ccfelius <lottefelius@hotmail.com>
2022-05-27 15:49:03 +0200

    python-is-python3 instead of python

--- ycsb/driver/scripts/install.sh
+++ ycsb/driver/scripts/install.sh
@@ -3,6 +3,7 @@
 export HOMEDIR=$(pwd)
 sudo apt install -y default-jre python postgresql-client-common postgresql-client-14
 
+sudo apt install python-is-python3 -y
 sudo apt-get install python3-pip -y
 pip3 install fire
 pip3 install pandas
