5ddae6ad940241a900ac5030c15d39aeef1b0d6c
ccfelius <lottefelius@hotmail.com>
2022-05-28 11:58:26 +0200

    added dynamic parallel

--- ycsb/driver/scripts/build-and-run-ycsb.sh
+++ ycsb/driver/scripts/build-and-run-ycsb.sh
@@ -1,3 +1,3 @@
 cd $HOMEDIR
 
-python3 benchmark.py --host=$PGHOST --parallel=True --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW citus_workload
+python3 benchmark.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW citus_workload
