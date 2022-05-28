cb6fab4a8beee3f2770a657aa2ae9c47408e1a1d
ccfelius <lottefelius@hotmail.com>
2022-05-28 08:53:31 +0200

    variable typo

--- ycsb/driver/scripts/build-and-run-ycsb.sh
+++ ycsb/driver/scripts/build-and-run-ycsb.sh
@@ -16,4 +16,4 @@ export PARALLEL=True
 
 cd $HOMEDIR
 
-python3 benchmark.py --host=PGHOST --parallel=$PARALLEL --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW citus_workload
+python3 benchmark.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW citus_workload
