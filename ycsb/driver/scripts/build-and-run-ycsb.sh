579199e100308c0093ae56a7643316f3bd882d8b
ccfelius <lottefelius@hotmail.com>
2022-05-28 13:13:26 +0200

    changed homedir to folder

--- ycsb/driver/scripts/build-and-run-ycsb.sh
+++ ycsb/driver/scripts/build-and-run-ycsb.sh
@@ -1,4 +1,4 @@
-cd $HOMEDIR
+cd $FOLDER
 
 echo Starting Benchmarks
 echo "python3 benchmark.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW citus_workload"
