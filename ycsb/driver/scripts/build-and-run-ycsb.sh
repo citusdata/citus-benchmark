a6c034ebaf928e33e3cf553f664cfd03519877ea
ccfelius <lottefelius@hotmail.com>
2022-05-28 14:00:11 +0200

    delete files

--- ycsb/driver/scripts/build-and-run-ycsb.sh
+++
@@ -1,6 +0,0 @@
-cd $FOLDER
-
-echo Starting Benchmarks
-echo "python3 benchmark.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW citus_workload"
-
-python3 benchmark.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW citus_workload
