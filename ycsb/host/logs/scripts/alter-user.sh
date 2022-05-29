478859dadf9f52205f7a7bc3dd82caff2059c56d
ccfelius <lottefelius@hotmail.com>
2022-05-29 22:16:15 +0200

    removed run_command_on_workers() as it did not work

--- ycsb/host/logs/scripts/alter-user.sh
+++ ycsb/host/logs/scripts/alter-user.sh
@@ -5,17 +5,11 @@ export HOST=$2
 export LOG_STATEMENT=$(echo $(cat log-statement))
 export LOG_DURATION=$(echo $(cat log-duration))
 export LOG_MIN_MESSAGES=$(echo $(cat log-min-messages))
-export LOG_STATEMENT_TWO=$(echo $(cat log-statement-2))
-export LOG_DURATION_TWO=$(echo $(cat log-duration-2))
-export LOG_MIN_MESSAGES_TWO=$(echo $(cat log-min-messages-2))
 export PERMISSIONS=$(echo $(cat permissions))
 
 ssh -t \
     -o "UserKnownHostsFile=/dev/null" \
     -o "StrictHostKeyChecking=no" \
     $PREFIX@$HOST "$LOG_STATEMENT; $LOG_MIN_MESSAGES; $LOG_DURATION; $PERMISSIONS"
-
-
-    # $PREFIX@$HOST "$LOG_STATEMENT; $LOG_STATEMENT_TWO; $LOG_DURATION; $LOG_DURATION_TWO; $LOG_MIN_MESSAGES; $LOG_MIN_MESSAGES_TWO; $PERMISSIONS"
 
 
