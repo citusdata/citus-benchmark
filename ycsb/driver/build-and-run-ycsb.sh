f4a5643f540490fa3a47f319f9079b4024afc6f7
ccfelius <lottefelius@hotmail.com>
2022-05-29 12:32:28 +0200

    tmux name of session to be killed

--- ycsb/driver/build-and-run-ycsb.sh
+++ ycsb/driver/build-and-run-ycsb.sh
@@ -3,4 +3,4 @@ echo "python3 benchmark.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORD
 
 python3 benchmark.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW citus_workload
 
-tmux kill-session
+tmux kill-session -t init-bench
