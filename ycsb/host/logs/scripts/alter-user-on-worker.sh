018eeb5814920a7bf923219441467a6557dba3e2
ccfelius <lottefelius@hotmail.com>
2022-05-29 22:16:45 +0200

    run on all workers for propagating permissions to user monitor

---
+++ ycsb/host/logs/scripts/alter-user-on-worker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export PREFIX=$1
+export HOST=$2
+export LOG_STATEMENT=$(echo $(cat log-statement))
+export LOG_DURATION=$(echo $(cat log-duration))
+export LOG_MIN_MESSAGES=$(echo $(cat log-min-messages))
+
+ssh -t \
+    -o "UserKnownHostsFile=/dev/null" \
+    -o "StrictHostKeyChecking=no" \
+    $PREFIX@$HOST "$LOG_STATEMENT; $LOG_MIN_MESSAGES; $LOG_DURATION; $PERMISSIONS"
+
+
