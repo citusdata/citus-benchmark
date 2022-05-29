5eab01c1ee80161d9468cf18c8493cd749ec274e
ccfelius <lottefelius@hotmail.com>
2022-05-29 22:14:12 +0200

    included iostat + explanation

--- ycsb/host/collect_data.py
+++ ycsb/host/collect_data.py
@@ -1,20 +1,11 @@
-# DRIVER FILE:
-# Waiting for driver: Checks every 10 seconds for a run.start file on driverVM
-# If run.start, starts background process worker node that runs iostat
-# collect these logs (nohup.out) every x seconds
-# Waiting for Driver: Checks every 60 seconds for a run.finished file on driverVM
-# If run.finalized, collect csv’s from driver(s) and raw YCSB output for every workload
 # Collects postgresql logs from /dat/14/data/pg_log/ produced by user ‘monitor’ on every worker/coordinator
 # Stores all collected files in RDS and S3 so that they persist
-# If succeeded, delete locally generated files and folders
-
+# If succeeded, delete locally generated files and folders (optional)
 
 import os
-import pandas as pd
 from helper import *
 import yaml
 from logs import Logging
-import time
 import sys
 
 homedir = os.getcwd()
@@ -32,6 +23,9 @@ with open('config.yml', 'r') as f:
 # Create a logging instance
 logs = Logging(resource = cluster['resource'], prefix = cluster['prefix'], host = cluster['host'], password = cluster['pgpassword'], port = cluster['port'], shard_count = ycsb['shard_count'])
 
+# Collect cpu usage from worker nodes
+logs.collect_iostat()
+
 # # Get csv's from driver # # Get raw ycsb-data from driver for every resource group and push to blob
 logs.get_csv_and_ycbs_logs()
 
@@ -48,13 +42,11 @@ run(["python3", 'push_to_db.py', path, "PARALLEL"], shell = False)
 # # Push to blob
 run(["./push-to-blob.sh", f"{path}/YCSB/raw/", bucket, f"{cluster['resource']}/raw/"], shell = False)
 run(["./push-to-blob.sh", f"{path}/pglogs/", bucket, f"{cluster['resource']}/pglogs"], shell = False)
+run(["./push-to-blob.sh", f"{path}/general/", bucket, f"{cluster['resource']}/general"], shell = False)
 
 # DELETE RESOURCE GROUP FOLDER
-# run(["rm", "-r", path], shell = False)
-# print("FOLDER REMOVED")
-# DELETE RESOURCE GROUP
-# os.chdir("")
-# run(["rm", "-r", path], shell = False)
+run(["rm", "-r", path], shell = False)
+print("FOLDER REMOVED")
 
 
 print("DONE WITH BENCHMARKS")
--- ycsb/host/collect_data_individual.py
+++ ycsb/host/collect_data_individual.py
@@ -1,20 +1,12 @@
-# DRIVER FILE:
-# Waiting for driver: Checks every 10 seconds for a run.start file on driverVM
-# If run.start, starts background process worker node that runs iostat
-# collect these logs (nohup.out) every x seconds
-# Waiting for Driver: Checks every 60 seconds for a run.finished file on driverVM
 # If run.finalized, collect csv’s from driver(s) and raw YCSB output for every workload
 # Collects postgresql logs from /dat/14/data/pg_log/ produced by user ‘monitor’ on every worker/coordinator
-# Stores all collected files in RDS and S3 so that they persist
-# If succeeded, delete locally generated files and folders
+# Stores all collected files in postgresql and blob so that they persist
 
 
 import os
-import pandas as pd
 from helper import *
 import yaml
 from logs import Logging
-import time
 import sys
 
 homedir = os.getcwd()
@@ -48,6 +40,7 @@ run(["./push-to-blob.sh", f"{path}/YCSB/raw/", bucket, f"{cluster['resource']}/r
 # DELETE RESOURCE GROUP FOLDER
 run(["rm", "-r", path], shell = False)
 print("FOLDER REMOVED")
+
 # DELETE RESOURCE GROUP
 # os.chdir("")
 # run(["rm", "-r", path], shell = False)
