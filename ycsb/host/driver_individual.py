d536aa342e437490152684c074a8f3cea5319478
ccfelius <lottefelius@hotmail.com>
2022-05-29 22:14:42 +0200

    simplified script

--- ycsb/host/driver_individual.py
+++ ycsb/host/driver_individual.py
@@ -1,16 +1,4 @@
-# DRIVER FILE:
-# Waiting for driver: Checks every 10 seconds for a run.start file on driverVM
-# If run.start, starts background process worker node that runs iostat
-# collect these logs (nohup.out) every x seconds
-# Waiting for Driver: Checks every 60 seconds for a run.finished file on driverVM
-# If run.finalized, collect csv’s from driver(s) and raw YCSB output for every workload
-# Collects postgresql logs from /dat/14/data/pg_log/ produced by user ‘monitor’ on every worker/coordinator
-# Stores all collected files in RDS and S3 so that they persist
-# If succeeded, delete locally generated files and folders
-
-
 import os
-import pandas as pd
 from helper import *
 import yaml
 from logs import Logging
@@ -35,10 +23,10 @@ logs = Logging(resource = cluster['resource'], prefix = cluster['prefix'], host 
 time.sleep(120)
 os.chdir(homedir + '/logs/scripts/')
 
-# # # If 'run.finished' then get all generated csv's from driver vm and store in db's
+# If 'run.finished' then get all generated csv's from driver vm and store in db's
 run(["./try-sign.sh", cluster['resource'], 'run.finished', '60'], shell = False)
+
 os.chdir(homedir)
-print("Benchmark finished. Data collection Phase")
 
 # Data collection
 run(['python3', 'collect_data_individual.py', bucket], shell = False)
