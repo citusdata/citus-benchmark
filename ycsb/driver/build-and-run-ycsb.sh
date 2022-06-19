64e77b85fd77039edd4d3da753056f90a1a6a98a
ccfelius <lottefelius@hotmail.com>
2022-06-19 20:06:01 +0200

    delete server session

--- ycsb/driver/build-and-run-ycsb.sh
+++ ycsb/driver/build-and-run-ycsb.sh
@@ -4,5 +4,5 @@ echo "python3 benchmark.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORD
 tmux new -s server -d; tmux send-keys -t server  'python3 orchestrator.py' Enter
 python3 benchmark.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW monitor_workloadc
 
-# tmux kill-session -t server
+tmux kill-session -t server
 tmux kill-session -t init-bench
