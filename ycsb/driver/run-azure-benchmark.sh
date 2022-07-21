dd332efe448a348e713b1e742a1fddb2d85a7b3d
ccfelius <lottefelius@hotmail.com>
2022-07-21 20:35:05 +0200

    removed host

--- ycsb/driver/run-azure-benchmark.sh
+++ ycsb/driver/run-azure-benchmark.sh
@@ -1,3 +1,1 @@
-python3 benchmark-azure.py --host="$PGHOST" --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP citus_workload
-
-
+python3 benchmark-azure.py --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP citus_workload
