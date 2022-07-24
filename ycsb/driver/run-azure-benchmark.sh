a35463d25d02adea02e354bfbceda1f953655295
ccfelius <lottefelius@hotmail.com>
2022-07-24 22:08:16 +0200

    changed benchmark-azure to benchmark

--- ycsb/driver/run-azure-benchmark.sh
+++ ycsb/driver/run-azure-benchmark.sh
@@ -1,1 +1,1 @@
-python3 benchmark-azure.py --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS $WORKLOAD_FUNCTION
+python3 benchmark.py --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS $WORKLOAD_FUNCTION
