e776bb56ebff24e51686425395216d2869f6e7c4
ccfelius <lottefelius@hotmail.com>
2022-09-05 17:28:33 +0200

    updated params

--- ycsb/driver/build-and-run-ycsb-double-driver.sh
+++ ycsb/driver/build-and-run-ycsb-double-driver.sh
@@ -2,9 +2,9 @@ echo Starting Benchmarks
 
 if [ ${PART} -eq 0 ]; then
 	tmux new -s server -d; tmux send-keys -t server  'python3 server-double.py' Enter
-	python3 monitor-double.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW --drivers=$DRIVERS --driver_id=$PART monitor_workloada
+	python3 monitor-double.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW --drivers=$DRIVERS --id=$PART monitor_workloada
 else
-	python3 monitor-double-client.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW --drivers=$DRIVERS --driver_id=$PART monitor_workloada
+	python3 monitor-double-client.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW --drivers=$DRIVERS --id=$PART monitor_workloada
 fi
 
 # tmux kill-session -t server
