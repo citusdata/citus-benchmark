fb5733cd344d540eb1345d9dc6b9f61ba099511b
ccfelius <lottefelius@hotmail.com>
2022-09-24 20:16:49 +0200

    workload c

--- ycsb/driver/build-and-run-ycsb.sh
+++ ycsb/driver/build-and-run-ycsb.sh
@@ -1,8 +1,8 @@
 echo Starting Benchmarks
-echo "python3 monitor.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW citus_workload"
+echo "python3 monitor.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW monitor_workloadc"
 
 tmux new -s server -d; tmux send-keys -t server  'python3 server.py' Enter
-python3 monitor.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW monitor_workloada
+python3 monitor.py --host=$PGHOST --parallel=$PARALLEL --records=$RECORDS --operations=$OPERATIONS --shard_count=$SHARD_COUNT --threads=$THREAD_COUNT --iterations=$ITERATIONS --workers=$WORKERS --resource=$RESOURCE_GROUP --monitorpw=$MONITORPW monitor_workloadc
 
 # tmux kill-session -t server
 # tmux kill-session -t init-bench
