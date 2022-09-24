72ddccc294769e36d3e3c17234d73b668fc90813
ccfelius <lottefelius@hotmail.com>
2022-09-24 19:18:05 +0200

    added heartbeat

--- ycsb/driver/monitor.py
+++ ycsb/driver/monitor.py
@@ -726,6 +726,21 @@ def initiate_benchmarks(event: Event):
         event.set()
 
 
+def heartbeat(event: Event, frequency = 60):
+
+    global states
+
+    while not event.is_set():
+
+        try:
+            send_with_pickle()
+
+        except:
+            logging.warning("Failed to send states to server")
+
+        time.sleep(frequency)
+
+
 if __name__ == '__main__':
 
     try:
@@ -736,22 +751,27 @@ if __name__ == '__main__':
         # State Thread, monitors state and communicates with socket
         states_thread = threading.Thread(target = monitor_states, args=([event]),  daemon = True)
 
+        # Heartbeat Thread, sends heartbeats to server
+        heartbeat = threading.Thread(target = heartbeat, args=([event]),  daemon = True)
+
         # Benchmark Thread
         benchmark_thread = threading.Thread(target = initiate_benchmarks, args=([event]))
 
         # Start Threads
         states_thread.start()
         benchmark_thread.start()
+        heartbeat.start()
 
         # wait until benchmarks are finished
         states_thread.join()
         benchmark_thread.join()
+        heartbeat.join()
 
 
     except KeyboardInterrupt:
 
-         run(['python3', 'output.py', "results.csv"], shell = False)
-
+         run(['python3', 'output.py', f"results"], shell = False)
+         sys.exit(1)
 
 
 
