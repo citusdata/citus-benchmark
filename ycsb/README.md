7fe0c8b840e32b1d0b3fe158b8a2b6369680b196
ccfelius <lottefelius@hotmail.com>
2022-09-28 19:49:17 +0200

    new branch for ycsb tooling on azure

---
+++ ycsb/README.md
@@ -0,0 +1,51 @@
+# Run YCSB benchmarks on Citus in Azure Managed Service
+
+### Requirements:
+
+For the benchmarks to run, both `python2`and `python3` are required. In addition, we make use of the Python package `python fire`.
+
+E.g. a script for Linux/Ubuntu
+
+```
+sudo apt -y install vim bash-completion wget
+sudo apt update -y
+sudo apt-get install python3-pip -y
+pip3 install fire
+```
+
+### Run YCSB against Citus on Azure
+
+The scripts load the amount of records specified behind the records flag and runs any of the YCSB standard workloads by performing the specified amount of operations. A CSV with some statistics from the benchmark is automatically generated afterwards on the driver VM and transferred to your local machine.
+
+For details on the flags run:
+
+```
+python3 ycsb.py --help
+```
+
+Example usage:
+
+```
+python3 ycsb.py --resource=yourname --records=10000 --operations=10000 --threads=100,200,300 --iterations=2 --workers=2 --workload="run_all_workloads" start_benchmark
+```
+
+##### Parameters explained
+
+> `resource` is the name of the resource group that will be created for running the benchmarks <br>
+> `records` the amount of records to be inserted by YCSB <br>
+> `operations` is the amount of operations performed by YCSB <br>
+> `threads` is a list (seperated by a comma) of the different threadcouns used during the YCSB benchmarks <br>
+> `shard_count` default is 2 * workers. You can set your own shard count with this parameter. <br>
+> `iterations` is the amount of iterations for the benchmarks <br>
+> `workers` represents the amount of workers in the Citus cluster <br>
+> `autodelete` default value is false. If set to true, cluster will be automatically deleted when benchmarks are finished. <br>
+> `workloads` are the YCSB workloads one can test. We have the following options: <br>
+>
+> > - workloada
+> > - workloadb
+> > - workloadc
+> > - workloadd
+> > - workloade
+> > - workloadf
+> > - run_all_workloads
+>
---
+++ ycsb/ycsb.py
@@ -0,0 +1,158 @@
+import sys
+import subprocess
+import os
+import datetime
+import fire
+import logging
+
+MAX_THREADS = 1000
+MIN_THREADS = 1
+CHECK_FREQUENCY_IN_SECONDS = 10
+
+def eprint(*args, **kwargs):
+
+    """
+    eprint prints to stderr
+    """
+
+    print(*args, file=sys.stderr, **kwargs)
+
+
+def run(command, *args, shell=True, **kwargs):
+
+    """
+    run runs the given command and prints it to stderr
+    """
+
+    eprint(f"+ {command} ")
+    result = subprocess.run(command, *args, check=True, shell=shell, **kwargs)
+
+    return result
+
+
+class StartBenchmark(object):
+
+    def check_if_thread_is_int(self, thread):
+
+        """
+        Check whether given input is a valid integer
+        """
+
+        global MAX_THREADS
+        global MIN_THREADS
+
+        try:
+                if int(thread) > MAX_THREADS:
+                    raise ValueError('Invalid input, threadcount exceeds maximum of {0}'.format(MAX_THREADS))
+
+                elif int(thread) < MIN_THREADS:
+                    raise ValueError('Invalid input, threadcount exceeds maximum of {0}'.format(MIN_THREADS))
+
+        except:
+                raise ValueError('Invalid input, please enter integers in format "300" or "300,400" if multiple threadcounts')
+
+        return thread
+
+
+    def parse_threadcounts(self, thread_counts):
+
+        """
+        Parses string of threadcounts and returns list
+        """
+
+        try:
+
+            int(thread_counts)
+            return self.check_if_thread_is_int(thread_counts)
+
+        except:
+            return ','.join([str(self.check_if_thread_is_int(thread)) for thread in thread_counts])
+
+
+    def set_shard_count(self, workers, shards):
+
+        if not shards:
+            return 2 * int(workers)
+
+        return shards
+
+
+    def __init__(self, resource, threads = "248", records = 1000, operations = 10000, database = "citus",  workload = "run_all_workloads",
+    iterations = 1, workers = "2", shard_count = 0, autodelete = False, deployment = "hyperscale-ycsb"):
+
+        self.THREADS = self.parse_threadcounts(threads)
+        self.RECORDS = records
+        self.OPERATIONS = operations
+        self.ITERATIONS = iterations
+        self.WORKERS = workers
+        self.SHARD_COUNT = self.set_shard_count(workers, shard_count)
+        self.DATABASE = database
+        self.RG = resource
+        self.DEPLOYMENT = deployment
+        self.AUTO = autodelete
+        self.WORKLOAD_FUNCTION = workload
+
+        # Set environment variables
+        os.environ['HOMEDIR'] = os.getcwd()
+        os.environ['PGDATABASE'] = self.DATABASE
+        os.environ['RESOURCE'] = self.RG
+        os.environ['SHARD_COUNT'] = str(self.SHARD_COUNT)
+        os.environ['ITERATIONS'] = str(self.ITERATIONS)
+        os.environ['WORKERS'] = str(self.WORKERS)
+        os.environ['RECORDS'] = str(self.RECORDS)
+        os.environ['THREADS'] = str(self.THREADS)
+        os.environ['WORKLOADS'] = str(self.WORKLOAD_FUNCTION)
+        os.environ['OPERATIONS'] = str(self.OPERATIONS)
+
+
+    @property
+    def resource(self):
+        """ get resource group """
+
+        return self.RG
+
+
+    @property
+    def deployment(self):
+        """ get deployment """
+
+        return self.DEPLOYMENT
+
+
+    @property
+    def autodelete(self):
+        """ get autodelete """
+
+        return self.AUTO
+
+
+    def start_benchmark(self):
+
+        """ initiates benchmark run and corresponding infrastructure """
+
+        global CHECK_FREQUENCY_IN_SECONDS
+
+        logging.info(datetime.datetime.now())
+
+        # change to azure directory
+        citus_folder = os.path.abspath(os.curdir)
+        os.chdir(citus_folder + "/azure")
+
+        # start the benchmark
+        run(['./start-benchmark-ycsb.sh', self.resource, self.deployment], shell = False)
+
+        try:
+            run(['./wait-for-results-ycsb.sh', 'benchmarks.finished', str(CHECK_FREQUENCY_IN_SECONDS)], shell = False)
+
+        except KeyboardInterrupt:
+            run(['./get-file.sh', f'{self.resource}-results.csv'], shell = False)
+
+        # Delete cluster if autodelete is set to true
+        if self.autodelete:
+                run(['./cleanup.sh', self.resource], shell = False)
+
+        os.chdir(citus_folder + "/ycsb")
+
+if __name__ == '__main__':
+
+    benchmark = fire.Fire(StartBenchmark)
